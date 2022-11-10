d28464c62b3bdff5ae5a9a0f797ca6caa503b7bd
hugos <asihkas@ashoij.com>
2022-11-10 17:55:08 +0100

    HOLa

--- KanBan.php
+++ KanBan.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./styles.css">
-    <title>Document</title>
+    <title>KanBan</title>
 </head>
 <body>
-    
+    <div id="Todo">
+
 </body>
 </html>
 
@@ -18,13 +19,21 @@
 //hola
     $list = array(
         "Backlog" => array(), //pene
-        "To Do" => array(),
+        "ToDo" => array("llista to do", "css maca"," nomes mostrar la llista"," com posar nou item ", "com eliminar nou item"),
         "Doing" => array(),
         "Done" => array()
         
     );
+    $cont = 0;
+    $cont2 = 0;
     foreach ($list as $key => $value) {
-        echo "$key <br>";
+        $cont++;
+        echo " <h3>$key</h3> <div id='$key'>";
+        foreach ($value as $k) {
+            $cont2++;
+            echo "<b>$cont2.</b> $k <br>";
+        }
+        echo"</div>";
     }
 
 
