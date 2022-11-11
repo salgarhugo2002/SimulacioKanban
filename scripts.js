c7209b93fd13dbc5556e632f559b86168f78624d
Mish <mgiro.dam@institutcampalans.net>
2022-11-11 17:52:24 +0100

    a

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
-document.getElementById("text1").value;
+function guardarDatos(){
+    let txtArea = document.getElementById("text1").value;
+    alert(txtArea);
+    
+}
+
+function eliminarDatos(){
+    let num = parseInt(document.getElementById("numero").value);
+    alert(num);
+}
