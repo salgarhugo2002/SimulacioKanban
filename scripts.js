fe7580b37784310143f0a5a9806cfbb295e298a4
Mish <mgiro.dam@institutcampalans.net>
2022-11-17 16:29:57 +0100

    a

--- scripts.js
+++ scripts.js
@@ -2,9 +2,17 @@
 var todo = [];
 
 function guardar() {
-  todo.push(document.getElementById("text1").value);  
-    
+    try{
+        let dato = document.getElementById("text1").value;
+        
+        if(dato == "" || dato == null){throw "No hi ha text que afegir"}
+        else{document.getElementById("text1").value = null; todo.push(dato); mostrar();}
+        
+    }catch(err){
+        alert(err);
     }
+
+}
 
 function mostrar(){
     var node = document.createElement('li');
@@ -13,6 +21,6 @@ function mostrar(){
         node = document.createElement('li');
         node.appendChild(document.createTextNode(element));
 
-       });
-       document.querySelector('ol').appendChild(node);
+    });
+    document.querySelector('ol').appendChild(node);
 }
