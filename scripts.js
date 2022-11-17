b655451b872725657c7fd5468a632a30c6a72ed0
hugos <asihkas@ashoij.com>
2022-11-17 15:28:28 +0100

    asd

--- scripts.js
+++ scripts.js
@@ -1,3 +1,18 @@
-<script>
+
+var backlog = [];
+
+function guardar() {
+    backlog.push(document.getElementById("text1").value )    
     
-</script>
+    mostrar();
+    }
+
+
+          
+function mostrar() {
+    document.getElementById("textbacklog").innerHTML += "<ol start='5'>"
+   backlog.forEach(element => {
+    document.getElementById("textbacklog").innerHTML += "<li> " + element + "</li>";
+   });
+   document.getElementById("textbacklog").innerHTML += "</ol>"
+}
