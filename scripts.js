d61738bb27eac886158cfa03c5e20945cda4a9b2
hugos <asihkas@ashoij.com>
2022-11-17 17:01:12 +0100

    asd

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
+
 var todo = [];
 
 function guardar() {
     try{
         let dato = document.getElementById("text1").value;
-
+        
         if(dato == "" || dato == null){throw "No hi ha text que afegir"}
         else{document.getElementById("text1").value = null; todo.push(dato); mostrar();}
         
@@ -14,7 +15,7 @@ function guardar() {
 }
 
 function mostrar(){
-    var node = document.createElement('li');
+       var node = document.createElement('li');
 
     todo.forEach(element => {
         node = document.createElement('li');
