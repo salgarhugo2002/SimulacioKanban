14cf850f30e002573bbecc60ad96bdaf5cdda600
hugos <asihkas@ashoij.com>
2022-11-17 15:48:58 +0100

    asd

--- scripts.js
+++ scripts.js
@@ -2,17 +2,28 @@
 var backlog = [];
 
 function guardar() {
-    backlog.push(document.getElementById("text1").value )    
+  backlog.push(document.getElementById("text1").value);  
     
-    mostrar();
+    mostrar2();
     }
 
 
           
 function mostrar() {
-    document.getElementById("textbacklog").innerHTML += "<ol start='5'>"
+    document.getElementById("textbacklog").innerHTML = "<ol type='I'>";
    backlog.forEach(element => {
     document.getElementById("textbacklog").innerHTML += "<li> " + element + "</li>";
    });
-   document.getElementById("textbacklog").innerHTML += "</ol>"
+   document.getElementById("textbacklog").innerHTML += "</ol>";
+}
+
+
+function mostrar2(){
+    var node = document.createElement('li');
+    backlog.forEach(element => {
+        node.appendChild(document.createTextNode(element));
+       });
+       document.querySelector('ol').appendChild(node);
+
+
 }
