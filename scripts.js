958a34a3670dc25fe018a8d8147671879ced9b2a
Mish <mgiro.dam@institutcampalans.net>
2022-11-18 16:41:16 +0100

    adsa

--- scripts.js
+++ scripts.js
@@ -6,14 +6,7 @@ function guardar() {
         let dato = document.getElementById("text1").value;
         
         if(dato == "" || dato == null){throw "No hi ha text que afegir"}
-        else
-        {
-            
-            //document.getElementById("text1").value = null;
-         
-            todo.push(dato); 
-         
-             mostrar();}
+        else{document.getElementById("text1").value = null; todo.push(dato); mostrar();}
         
     }catch(err){
         alert(err);
