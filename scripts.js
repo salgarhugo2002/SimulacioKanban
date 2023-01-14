a0398815df957598b159d0245998c72e42be0679
Mish <mgiro.dam@institutcampalans.net>
2023-01-14 20:48:12 +0100

    DASD

--- scripts.js
+++ scripts.js
@@ -4,9 +4,11 @@ var codi = [1];
 var idresponsable = [1];
 var responsables = [];
 
+
 /* És una classe que crea un objecte de tasca amb un títol, text, codi, data de creació, data de venciment,
 responsable, llista i prioritat. */
 class Task {
+
     constructor(_titols, _textp, _codip, data1, data, responsables, listas, prioridad) {
         this._titol = _titols;
         this._text = _textp;
@@ -17,7 +19,6 @@ class Task {
         this._Lista = listas;
         this._prioridad = prioridad
     }
-
     RetornTitol() {
         return this._titol
     }
@@ -25,7 +26,6 @@ class Task {
     Retorncodi() {
         return this._codi;
     }
-
     set codi(codi2) {
         this.codi = codi2;
     }
@@ -42,6 +42,7 @@ class Task {
         return this._text;
     }
 
+
     RetornResponsable() {
         return this._responsable;
     }
@@ -56,49 +57,53 @@ class Task {
     Prio() {
         return this._prioridad
     }
-}
 
+
+}
 /* Crea una classe anomenada Responsable. */
 class Responsable {
+
     constructor(_ids, _noms) {
-        _id = _ids;
-        _nom = _noms;
+        this._id = _ids;
+        this._nom = _noms;
 
     }
-
     RetornId() {
-        return _id;
+        return this._id;
     }
-
     set Id(a) {
-        _id = a;
+        this._id = a;
     }
 
     RetornNom() {
-        return _nom;
+        return this._nom;
     }
-
     set Nom(a) {
-        _nom = a;
+        this._nom = a;
     }
 }
+
 
 /* Carregant l'emmagatzematge local. */
 carregarlocal();
-
 /* Afegeix 1 al primer element de la matriu codi. */
+afegirUsuaris()
+
 function Generarid() {
     codi[0] = codi[0] + 1;
 }
-
 /* Generar un nou ID per a la persona responsable. */
+
 function GenerarIdResponsable() {
     idresponsable[0] = idresponsable[0] + 1;
 }
 
+
 /* Agafa els valors dels inputs i crea una tasca nova, introduint-la a la matriu. */
+
 function guardarToDo() {
     /* Try-catch que detecta si algun titol esta repetit o i ha algun cap buit. */
+
     try {
         let responsable = document.getElementById('resp').value
         let titol = document.getElementById("titol1").value;
@@ -107,9 +112,11 @@ function guardarToDo() {
         let data1 = new Date().toLocaleDateString();
         let prio = document.getElementById('pri').value
 
+
         if (!validarTitol()) {
             throw "No pots repetir titol";
-        }else {
+        } else {
+
             if (dato == "" || dato == null) {
                 throw "Inserta un text";
             }
@@ -117,24 +124,31 @@ function guardarToDo() {
                 if (titol == "" || titol == null) {
                     throw "Inserta un títol";
                 } else {
+
                     document.getElementById("text1").value = null;
                     document.getElementById("titol1").value = null;
                     let tasca = new Task(titol, dato, codi[0], data1, data, retornideresponsable(responsable), "ToDo", prio);
                     Generarid();
+
                     todo.push(tasca);
+
 
                     mostrar();
                     guardarlocal();
                 }
+
             }
         }
+
     } catch (err) {
         alert(err);
     }
-}
 
+}
 /* Comprova si el títol de la tasca nova ja és a la llista de tasques. Retorna un valor booleà. */
+
 function validarTitol() {
+
     let bool = true;
     for (element of todo) {
         if (element.RetornTitol() == document.getElementById("titol1").value) {
@@ -146,10 +160,11 @@ function validarTitol() {
         }
     };
 
+
     return bool;
 }
-
 /* Crea un element de llista per a cada element de la matriu i l'afegeix a la llista adequada */
+
 function mostrar() {
     var node;
     document.getElementById('listaToDo').innerHTML = '';
@@ -163,16 +178,17 @@ function mostrar() {
         node.className = "task"
         node.id = cont
         if (element.Prio() == "Alta") {
-            node.style.backgroundColor = "#ff2c2c"
-        }else if(element.Prio() == "Normal"){
-            node.style.backgroundColor = "#fcca42"
+            node.style.backgroundColor = "red"
+        } else if (element.Prio() == "Normal") {
+            node.style.backgroundColor = "cyan"
         }
         else if (element.Prio() == "Baixa") {
             node.style.backgroundColor = "lightgreen"
         }
         cont++;
 
-        node.appendChild(document.createTextNode(element.RetornTitol() + " "));
+        node.appendChild(document.createTextNode(element.RetornTitol() + "." + " "));
+        node.appendChild(document.createTextNode(element.RetornTitol()));
 
         if (element.RetornLista() == "ToDo") {
             document.querySelector('#listaToDo').appendChild(node);
@@ -182,14 +198,20 @@ function mostrar() {
 
             document.querySelector('#listaDone').appendChild(node);
         }
+
+
     });
+
 }
 
-/* Pren una cadena com a argument, i si aquesta cadena coincideix amb el títol d'un objecte de 'todo', elimina aquest objecte de la matriu de 'todo'.*/
-function eliminarToDo(text) {
+function eliminarToDo(text = "") {
     let cont = 0;
+    let pos = text.indexOf('.')
+    let text2 = text.substring(0, pos)
     todo.forEach(element => {
-        if (text.trim() == element.RetornTitol()) {
+        if (text2.trim() == element.RetornTitol()) {
+
+
             todo.splice(cont, 1);
 
         } else {
@@ -203,13 +225,16 @@ function eliminarToDo(text) {
 }
 
 /* Pren les dades de la matriu 'todo', la variable codi i, les desa a l'emmagatzematge local. */
+
 function guardarlocal() {
 
     localStorage.setItem('llista', JSON.stringify(todo));
     localStorage.setItem('id', JSON.stringify(codi));
     localStorage.setItem('idresponsable', JSON.stringify(idresponsable));
-    localStorage.setItem('responsables', JSON.stringify(responsables));
+    localStorage.setItem('responsables', JSON.stringify(responsables))
+
 }
+
 
 function carregarlocal() {
     if (typeof (Storage) !== "undefined") {
@@ -220,14 +245,14 @@ function carregarlocal() {
                 todo.push(new Task(element._titol, element._text, element._codi, element._data_creacio, element._data_previsio_finalitzacio, element._responsabl, element._Lista, element._prioridad));
             });
             mostrar();
-        }else{
+        } else
             localStorage.setItem('llista', JSON.stringify(todo));
-        }
-        if (localStorage.id){
+
+        if (localStorage.id)
             codi = JSON.parse(localStorage.getItem('id'));
-        }else{
+        else
             localStorage.setItem('id', JSON.stringify(codi));
-        }
+
         if (localStorage.idresponsable)
             idresponsable = JSON.parse(localStorage.getItem('idresponsable'));
         else
@@ -236,7 +261,7 @@ function carregarlocal() {
 
             let dataresponsables = [] = JSON.parse(localStorage.getItem('responsables'))
             dataresponsables.forEach(element => {
-                responsables.push(new Responsable(element._nom))
+                responsables.push(new Responsable(element._id,element._nom))
             });
         }
         else
@@ -245,8 +270,8 @@ function carregarlocal() {
         alert("Sorry, your browser does not support web storage...");
 
     }
-}
 
+}
 /* Si es prem la tecla Intro, a continuació, activarà l'event 'clic al botó'
 amb l'id 'btng'. */
 document.getElementById("text1").addEventListener("keypress", function (event) {
@@ -255,6 +280,7 @@ document.getElementById("text1").addEventListener("keypress", function (event) {
         document.getElementById("btng").click();
     }
 })
+
 
 /* Creació de variables per als diferents divs i llistes. */
 const dragToDo = document.getElementById('divToDo')
@@ -265,9 +291,7 @@ const listaDoing = document.getElementById('listaDoing')
 const listaToDo = document.getElementById('listaToDo')
 const listaDone = document.getElementById('listaDone')
 
-
 /*CODI PER CREAR EL SISTEMA D'ARROSSEGAMENT DE LES TASQUES TODO */
-
 dragToDo.addEventListener('dragstart', (e) => {
     e.dataTransfer.setData('text/plain', e.target.id)
 })
@@ -283,18 +307,6 @@ dragToDo.addEventListener('dragend', (e) => {
 dragToDo.addEventListener('dragover', (e) => {
     e.preventDefault()
 })
-
-dragToDo.addEventListener('drop', (e) => {
-    e.preventDefault()
-    const element = document.getElementById(e.dataTransfer.getData('text'))
-    element.classList.remove('active')
-    listaToDo.appendChild(listaDoing.removeChild(element))
-
-    a = element.innerHTML;
-    CambiarLista(a, "ToDo")
-
-})
-
 
 /*CODI PER CREAR EL SISTEMA D'ARROSSEGAMENT DE LES TASQUES DOING */
 
@@ -314,20 +326,8 @@ dragDoing.addEventListener('dragover', (e) => {
     e.preventDefault()
 })
 
-dragDoing.addEventListener('drop', (e) => {
-    e.preventDefault()
-    const element = document.getElementById(e.dataTransfer.getData('text'))
-    element.classList.remove('active')
-    listaDoing.appendChild(listaToDo.removeChild(element))
-
-    a = element.innerHTML;
-    CambiarLista(a, "Doing")
-
-})
-
 
 /*CODI PER CREAR EL SISTEMA D'ARROSSEGAMENT DE LES TASQUES DONE */
-
 dragDone.addEventListener('dragstart', (e) => {
     e.dataTransfer.setData('text/plain', e.target.id)
 })
@@ -344,6 +344,32 @@ dragDone.addEventListener('dragover', (e) => {
     e.preventDefault()
 })
 
+
+
+
+/*CODI PER CREAR EL SISTEMA D'ARROSSEGAMENT DE LES TASQUES A LA PAPERERA */
+dragDoing.addEventListener('drop', (e) => {
+    e.preventDefault()
+    const element = document.getElementById(e.dataTransfer.getData('text'))
+    element.classList.remove('active')
+    listaDoing.appendChild(listaToDo.removeChild(element))
+
+    a = element.innerHTML;
+    CambiarLista(a, "Doing")
+
+})
+
+dragToDo.addEventListener('drop', (e) => {
+    e.preventDefault()
+    const element = document.getElementById(e.dataTransfer.getData('text'))
+    element.classList.remove('active')
+    listaToDo.appendChild(listaDoing.removeChild(element))
+
+    a = element.innerHTML;
+    CambiarLista(a, "ToDo")
+
+})
+
 dragDone.addEventListener('drop', (e) => {
     e.preventDefault()
     const element = document.getElementById(e.dataTransfer.getData('text'))
@@ -355,8 +381,22 @@ dragDone.addEventListener('drop', (e) => {
 
 })
 
+dragDoing.addEventListener('drop', (e) => {
+    e.preventDefault()
+    const element = document.getElementById(e.dataTransfer.getData('text'))
+    element.classList.remove('active')
+    listaDoing.appendChild(listaDone.removeChild(element))
 
-/*CODI PER CREAR EL SISTEMA D'ARROSSEGAMENT DE LES TASQUES A LA PAPERERA */
+
+    a = element.innerHTML;
+    CambiarLista(a, "Doing")
+
+})
+
+
+
+
+
 
 papelera.addEventListener('dragstart', (e) => {
     e.dataTransfer.setData('text/plain', e.target.id)
@@ -385,40 +425,86 @@ papelera.addEventListener('drop', (e) => {
 
 })
 
-/* Funció que agafa el títol i la llista d'un element i el canvia a una altra llista */
-function CambiarLista(text, lista) {
+function CambiarLista(text = "", lista) {
+    let pos = text.indexOf('.')
+
+    let text2 = text.substring(0, pos)
+    text = text.trim()
+
     todo.forEach(element => {
-        if (text.trim() == element.RetornTitol()) {
+        if (text2 == element.RetornTitol()) {
+
             element.setLista(lista);
+
         }
+
     });
 
     guardarlocal();
+
 }
 
-/* Funció que serveix per guardar els responsables */
+
+
 function guardarresponsable() {
     nom = document.getElementById('nomresponsable').value
-    id = GenerarIdResponsable();
+    GenerarIdResponsable();
 
 
-    let resp = new Responsable(nom, id)
+    let resp = new Responsable(idresponsable[0],nom)
+
 
     responsables.push(resp);
+    document.getElementById('nomresponsable').value = ""
+    guardarlocal()
+    afegirUsuaris()
 }
 
-/* Funció que retorna l'id del responsable */
+
+
+function eliminarResponsable(){
+   let cont = 0
+    responsables.forEach(element =>{
+        if (element.RetornId() == parseInt(document.getElementById('borrarResp').value) ) {
+            responsables.splice(cont, 1);
+
+        } else {
+            cont++;
+        }
+    })
+    afegirUsuaris()
+    guardarlocal()
+}
+
+
 function retornideresponsable(resp) {
     let id = 0;
     responsables.forEach(element => {
         if (resp == element.RetornNom()) {
-            id = element.Retorncodi();
+            id = element.RetornId();
         }
 
     })
     return id;
+
 }
 
-/* Calendari */
+function afegirUsuaris(){
+  
+    document.getElementById('resp').innerHTML = ""
+
+    responsables.forEach(element =>{
+
+        var responsables2 = document.getElementById('resp')
+        var txt = element.RetornNom()
+        var o = document.createElement("option")
+        o.text = txt
+        responsables2.add(o)
+        
+    })
+}
+
+
 document.getElementById('PrevFinalitzacio').value = new Date().toISOString().split('T')[0]
+
 document.getElementById('PrevFinalitzacio').min = new Date().toISOString().split('T')[0]
